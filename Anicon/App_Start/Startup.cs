b815b4ab8e51821540f89dc5e40b3ef2ad967e33
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2022-10-03 10:28:55 -0300

    Começo do desenvolvimento back-end

---
+++ Anicon/App_Start/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using Microsoft.Owin.Security.Cookies;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Anicon.App_Start.Startup))]
+
+namespace Anicon.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AuthenticationType = "AppAplicationCookie",
+            //    LoginPath = new PathString("Home/Login")
+            //});
+        }
+    }
+}
